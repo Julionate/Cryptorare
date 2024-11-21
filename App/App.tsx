a7b5b86c778ebe1dc605361dcb8a63adcc6e930f
Julionate <julionate@proton.me>
2024-11-20 22:47:25 -0300

    Modificación de reglas TS

--- App/App.tsx
+++ App/App.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { Blurred } from "./components/Blurred";
 import { CurrencySelector } from "./components/CurrencySelector";
-import background from "../assets/wallhaven.jpg";
+import background from "../assets/holy.jpg";
 import "../global.css";
 
 export default function App() {
@@ -25,22 +25,27 @@ export default function App() {
         onLoadEnd={() => setTimeout(() => setLoading(false), 500)}
         className="flex-1 w-full h-full object-cover absolute"
       />
+
       <Text className="text-5xl text-white font-bold pb-10">Cryptorare</Text>
+
       <View className="flex flex-row justify-center items-center gap-2">
         <Blurred>
           <TextInput
             onChangeText={(text) => setTextInput(text)}
             onSubmitEditing={() => console.log("subiendo")}
             keyboardType="numeric"
-            className="w-80 h-16 flex px-2 text-xl text-white placeholder:text-white/45"
+            className="w-80 h-16 flex px-2 text-xl text-white placeholder:opacity-50"
+            placeholderTextColor="white"
             placeholder="Ingrese un valor"
           ></TextInput>
         </Blurred>
+
         <CurrencySelector
           currency={currentCurrency}
           setCurrentCurrency={setCurrentCurrency}
         />
       </View>
+
       <Blurred>
         <Pressable
           className="w-56 h-16 rounded-xl flex  justify-center items-center"
@@ -49,7 +54,9 @@ export default function App() {
           <Text className="text-xl text-white">Comparar</Text>
         </Pressable>
       </Blurred>
+
       <StatusBar style="light" />
+
       {loading ? (
         <View className="flex-1 items-center justify-center h-full w-full absolute bg-white z-50">
           <Text className="text-5xl text-black font-bold pb-10">
